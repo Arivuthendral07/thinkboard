f1e1c5328d908f2d78a309643059140428f71d49
Arivuthendral07 <21ada07@karpagamtech.ac.in>
2025-09-20 09:43:09 +0530

    Prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors"
-
+import path from "path"
 
 dotenv.config();
 
@@ -13,13 +13,15 @@ dotenv.config();
 
 const app=express();
 const PORT=process.env.PORT || 5001
-
+const __dirname=path.resolve()
 
 
 //middleware
+if(process.env.NODE_ENV !== "production"){
 app.use(cors({
     origin: "http://localhost:5173",
 }))
+}
 app.use(express.json());
 app.use(rateLimiter)
 
@@ -28,6 +30,15 @@ app.use(rateLimiter)
 //  next();
 //})
 app.use("/api/notes",notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+
+app.get("*",(req,res) =>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+}); 
+}
 
 
 //app.get("/api/notes",(req,res)=>{
